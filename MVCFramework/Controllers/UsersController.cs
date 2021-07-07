301141e0bb07ac515aa36ae6d90f1b8919c0679f
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-07-07 07:22:04 +0530

    [Feat]:Jwt Authentication Started✨

--- MVCFramework/Controllers/UsersController.cs
+++ MVCFramework/Controllers/UsersController.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MVCFramework.Controllers
 {
---
+++ MVCFramework/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MVCFramework.Startup))]
+
+namespace MVCFramework
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseJwtBearerAuthentication(
+               new JwtBearerAuthenticationOptions
+               {
+                   AuthenticationMode = AuthenticationMode.Active,
+                   TokenValidationParameters = new TokenValidationParameters()
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = "http://mysite.com", //some string, normally web url,  
+                        ValidAudience = "http://mysite.com",
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_secret_key_12345"))
+                   }
+               });
+        }
+    }
+}
